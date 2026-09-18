16af7ce1586ee5fc908fc6c4eb7303f58922d7c9
Priyanka Singh <113434149+Priyanka8967@users.noreply.github.com>
2026-09-18 20:36:18 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+       int [] ans =  new int[(nums.length)*2];
+
+        for(int i=0; i<ans.length; i++){
+
+            if(i > nums.length-1){
+                int j=i-nums.length;
+                ans[i] = nums[j];
+            }else{
+                ans[i] = nums[i];
+            }
+
+        }
+        return ans;
+    }
+}
